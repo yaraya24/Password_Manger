7f4189ee4b16ff74ef25095edf28fe25a126f191
Yonnas_Araya <yonnas.a24@hotmail.com>
2020-07-10 11:55:14 +0000

    Created users dir and added function to add users to create a csv file with hashed password and Salt appended

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from colorama import Fore, Back, Style
 from cryptography.fernet import Fernet
-import bcrypt, base64, re
-
+import bcrypt, base64, re, string, random
 from pathlib import Path
 import click, csv
 import os 
 import hashlib
+
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
 def main():
     while True:
@@ -78,20 +81,33 @@ def main():
                 authenticated = verify_master_password(master_password, user_name)
               
                 if authenticated:
-                    print(Fore.RED + "\n Successfully Logged In", Fore.WHITE)
+                    print(Fore.GREEN + "\n Successfully Logged In", Fore.WHITE)
                     while True:
                         print("""
                         Welcome to your security Vault.....
 
-                        1. add
+                        1. Add
                         2. Edit
                         3. Display
                         4. Logout
 
                         """)
-                        user_instruction = input(":")
-                        if user_instruction.lower() == "add" or user_instruction == '1':
-                            security_name = input("Please ")
+                        user_instruction = input(":").lower()
+                        if user_instruction == "add" or user_instruction == '1':
+                            service_name = input("Please enter the name of the service you wish to add to your vault - or enter 'return' to go back - \n:").capitalize()
+                            if service_name == 'return':
+                                continue
+                            elif check_for_duplicate_service(user_name, service_name):
+                                confirmation = input(Fore.YELLOW + f"Enter 'yes' if you wish to add {service_name} to your vault - A password will be auto-generated for you\n:" + Fore.WHITE).lower()
+                                if confirmation == 'yes' or confirmation == 'y':
+                                    service_password = password_generator()
+                                    add_service(master_password, user_name, service_name, service_password)
+                                    print(Fore.GREEN + f"{service_name} has been added to your vault", Fore.WHITE)
+                                else:
+                                    continue
+
+                            else:
+                                print(Fore.RED + "You already have a service with that name", Fore.WHITE)
 
                     break   
                 else:
@@ -99,8 +115,23 @@ def main():
                     continue
 
 
-    
+def read_csv_to_list(username):
+    csv_list = []
+    with open('users/' + username + '.csv', 'r') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            csv_list.append(row)
+        
+    return csv_list   
 
+def check_for_duplicate_service(username, name):
+    csv_list = read_csv_to_list(username)
+    for row in csv_list:
+        if name in row:
+            return False
+        else:
+            continue
+    return True
 
 def hashing_function(password, username):
     salt = base64.b64encode(os.urandom(32))
@@ -149,8 +180,36 @@ def password_complexity_checker(password):
     else:
         return True
 
+def password_generator():
+    length = 10
+    lower_letters = list(string.ascii_lowercase)
+    upper_letters = list(string.ascii_uppercase)
+    numbers = list(string.digits)
+    special_characters = list('!@#$%^&*')
+    total_possible_selection = lower_letters + upper_letters + numbers + special_characters
+    password = random.choice(lower_letters)
+    password += random.choice(upper_letters)
+    password += random.choice(numbers)
+    password += random.choice(special_characters)
+    for x in range(length):
+         password += random.choice(total_possible_selection)
+    return password
 
-            
+def add_service(master_password, username, name, specific_password):
+    csv_read_list = read_csv_to_list(username)
+    salt = csv_read_list[1][1].encode()
+   
+    kdf = PBKDF2HMAC(algorithm=hashes.SHA256(), length=32, salt=salt, iterations=100000, backend=default_backend())
+    encoding_key = base64.urlsafe_b64encode(kdf.derive(master_password.encode()))
+
+    encoded_password = Fernet(encoding_key).encrypt(specific_password.encode())
+    '***encoded_name = Fernet(encoding_key).encrypt(name.encode)****   -  UNSURE ABOUT NAME BEING ENCRYPTED JUST YET - HARD TO READ'
+
+    with open('users/' + username + '.csv', 'a', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow([name, encoded_password.decode()])
+
+
 if __name__ == '__main__':
     main()
 
